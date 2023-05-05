86a064deea441f41725c4f6eba4084c0129fb780
marvinbeckert <marvin.beckert@yareto.de>
2023-05-05 11:01:45 +0200

    Standalone Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ng-playground';
