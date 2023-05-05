a4d9d9b1c33056adeb7af3523cdee09f18cb75c7
marvinbeckert <marvin.beckert@yareto.de>
2023-05-05 11:40:30 +0200

    Playing with signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Signal, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { interval, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    MatIconModule,
-    MatButtonModule
-  ],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'ng-playground';
+
+  from = signal(0);
+  constructor() {
+    interval(1000).subscribe(() => this.updateValue());
+  }
+
+  updateValue() {
+    console.log('helo');
+    this.from.update((v) => {
+      return v + 1;
+    });
+  }
 }
